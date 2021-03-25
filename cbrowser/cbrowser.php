51b6eb8ca46a5d99091abe4abf33cdb0dc3f6c5b
Allan De Souza Melo <37338692+allansm@users.noreply.github.com>
2021-03-25 19:49:24 -0300

    ...

--- cbrowser/cbrowser.php
+++ cbrowser/cbrowser.php
@@ -12,15 +12,43 @@ function getPage($url){
 		unlink("phantom.txt");
 	}
 	$phantom = file("phantom");
-	$i = array_search("\$url",$phantom);
-	$phantom[$i] = str_replace("\$url", "'".$url."'", $phantom[$i]);
+	//$i = array_search("@url",$phantom);
+	//print($i);
+	$phantom[1] = str_replace("@url", "'".$url."'", $phantom[1]);
 	
 	foreach($phantom as $line){
 		removeLineBreak($line);
-		file_put_contents("phantom.txt", $line."\n", FILE_APPEND);
+		file_put_contents("phantom.txt", $line, FILE_APPEND);
 	}
 	
-	//exec("phantomjs phantom.txt");	
+	exec("phantomjs phantom.txt");
 }
 
-getPage("www.google.com");
+function splitbt($txt,$p1,$p2){
+	$pattern = "/[$p1$p2]/";
+
+	//$string = "something here ; and there, oh,that's all!";
+
+	//echo '<pre>', print_r( , 1 ), '</pre>';
+	return preg_split($pattern,$txt);
+}
+//$imp = implode("",file("dump.txt"));
+//preg_match('/href="(.*)"/', $imp, $match);//preg_replace('/<a [^>]*href="(.+)"/', '$1-123', $imp);
+
+//print_r($match);
+$exp = explode("http",implode("",file("dump.txt")));
+
+$dom = new DOMDocument();
+$html = implode("",file("dump.txt"));
+$dom->loadHTML($html);
+$nodes = $dom->getElementsByTagName("a");
+foreach ($nodes as $node) {
+      //echo $node->nodeValue."\n";
+	  print_r($node);
+  }
+//echo splitbt($imp,"href=\"","\"")[4];
+/*foreach(splitbt($imp,"href=\"","\"") as $fd){
+	print($fd);
+}*/
+
+//getPage("http://www.google.com");
