6e5483ed588d553fb13e8cd75285e69c6b25a9c5
Allan De Souza Melo <37338692+allansm@users.noreply.github.com>
2021-03-25 20:52:58 -0300

    ...

--- cbrowser/cbrowser.php
+++ cbrowser/cbrowser.php
@@ -36,16 +36,40 @@ function splitbt($txt,$p1,$p2){
 //preg_match('/href="(.*)"/', $imp, $match);//preg_replace('/<a [^>]*href="(.+)"/', '$1-123', $imp);
 
 //print_r($match);
-$exp = explode("http",implode("",file("dump.txt")));
+function linkExtractor($html){
+	$linkArray = array();
+	if(preg_match_all('/<a\s+.*?href=[\"\']?([^\"\' >]*)[\"\']?[^>]*>(.*?)<\/a>/i', $html, $matches, PREG_SET_ORDER)){
+		foreach ($matches as $match) {
+			array_push($linkArray, array($match[1], $match[2]));
+		}
+	}
+	return $linkArray;
+}
 
-$dom = new DOMDocument();
+function GetBetween($content,$start,$end)
+{
+    $r = explode($start, $content);
+    if (isset($r[1])){
+        $r = explode($end, $r[1]);
+        return $r;
+    }
+    return array();
+}
+
+
+
+$exp = implode("",file("dump.txt"));
+
+print_r(GetBetween($exp,"href=\"","\""));
+
+/*$dom = new DOMDocument();
 $html = implode("",file("dump.txt"));
 $dom->loadHTML($html);
 $nodes = $dom->getElementsByTagName("a");
 foreach ($nodes as $node) {
       //echo $node->nodeValue."\n";
 	  print_r($node);
-  }
+  }*/
 //echo splitbt($imp,"href=\"","\"")[4];
 /*foreach(splitbt($imp,"href=\"","\"") as $fd){
 	print($fd);
