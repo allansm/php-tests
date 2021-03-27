399e62b52ddbdbab1e845d56cf0283d3029e2546
Allan De Souza Melo <37338692+allansm@users.noreply.github.com>
2021-03-27 17:31:57 -0300

    ...

--- cbrowser/cbrowser.php
+++ cbrowser/cbrowser.php
@@ -104,6 +104,34 @@ function storeDump($fn,$url){
 				exec($tmp);
 			}
 		}
+		else if(!(strpos(strtolower($command)," : ") === false)){
+			unset($tmp);
+			$tmp = explode(" : ",$command);
+			$i = 0;
+			foreach($lines as $line){
+				if($i >= intval($tmp[0]) && $i <= intval($tmp[1])){
+					print($i++.":".$line."\n\n");
+				}
+			}
+		}
+		else if(!(strpos(strtolower($command)," - ") === false)){
+			unset($tmp);
+			$tmp = explode(" - ",$command);
+			$i = 0;
+			$new = array();
+			$arri = 0;
+			foreach($lines as $line){
+				if($i >= intval($tmp[0]) && $i <= intval($tmp[1])){
+					$new[$arri++] = $line;
+					print($i++.":".$line."\n\n");
+				}
+			}
+			unset($lines);
+			$lines = $new;
+		}
+		else if(strtolower($command) == "download"){
+			
+		}
 		
 		else if(strtolower($command) == "exitpage"){
 			//break;
@@ -142,81 +170,6 @@ while(true){
 				break;
 			}
 		}
-		/*$dump = file(temp2);
-		$lines = $dump;
-		while(true){
-			
-			$url = isset($url)?$url:temp2;
-			$command = readline($url.">");
-			if(strtolower($command) == "find"){
-				$tmp = implode("",$dump);
-				
-				$p1 =  readline("start:");
-				$p2 =  readline("end:");
-				
-				$found = find($tmp,$p1,$p2);
-				$i = 0;
-				foreach($found as $arr){
-					$lines[$i] = $arr[0];
-					print($i++.":".$arr[0]."\n\n");
-				}
-			}
-			else if(strtolower($command) == "showlines"){
-				$i = 0;
-				foreach($lines as $line){
-					print($i++.":".$line."\n\n");
-				}
-			}
-			
-			else if(strtolower($command) == "redirecttoline"){
-				$url = $lines[readline("line:")];
-				$redirect = true;
-				break;
-			}
-			else if(strtolower($command) == "addtolines"){
-				$add = readline("text to add:");
-				$op = readline("start or end(s/e)?");
-				$tmp = array();
-				if($op == "s"){
-					$i = 0;
-					foreach($lines as $line){
-						$tmp[$i++] = $add.$line;
-					}
-				}else{
-					$i = 0;
-					foreach($lines as $line){
-						$tmp[$i++] = $line.$add;
-					}
-				}
-				$lines = $tmp;
-			}
-			
-			else if(strtolower($command) == "lines > dump"){
-				$f = fopen(temp2, 'w');
-				$tmp = implode("",$lines);
-				fwrite($f, $tmp);
-				fclose($f);
-			}
-			
-			else if(!(strpos(strtolower($command),"lines >") === false)){
-				unset($tmp);
-				$tmp = explode(" > ",$command)[1];
-				if(isset($tmp)){
-					$f = fopen($tmp, 'w');
-					$tmp = implode("",$lines);
-					fwrite($f, $tmp);
-					fclose($f);
-					print("stored.\n");
-				}
-			}
-			
-			else if(strtolower($command) == "exitpage"){
-				break;
-			}
-			else if(strtolower($command) == "exit"){
-				die("bye :D");
-			}
-		}*/
 	}
 	else if(strtolower($command) == "open"){
 		$fn = readline("file path:");
