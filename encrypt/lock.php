7e5060d1cf1d5bf51bf66277d42a59c9483bfbda
allansm <allan.de.souza.melo.rj@gmail.com>
2021-04-13 20:20:09 -0300

    ...

--- encrypt/lock.php
+++ encrypt/lock.php
@@ -2,19 +2,15 @@
 
 include("encrypt.php");
 
-function test(){
-	//$file = file(readline("file to test:"));
+function unlock(){
 	$key = readline("key:");
 	$delimiter = readline("delimiter");
 	
-	//$enc = usePattern(readline("file:"),$key,$delimiter);
-
 	$file = file(readline("file:"));
 		
 	$op = readline("save ?(s/n):");
 
 	if($op == "s"){
-		//file_put_contents(readline("file:"));
 		$fname = readline("file:");
 		
 		foreach($file as $line){
@@ -22,9 +18,8 @@ function test(){
 		}
 	}else{
 		foreach($file as $line){
-			//file_put_contents("enc.txt",usePattern($line,$key,$delimiter)."\n",FILE_APPEND);
 			echo usePattern($line,$key,$delimiter);
 		}
 	}
 }
-test();
+unlock();
--- encrypt/unlock.php
+++ encrypt/unlock.php
@@ -2,24 +2,22 @@
 
 include("encrypt.php");
 
-function test(){
+function lock(){
 	$file = file(readline("file:"));
 	$key = readline("key:");
 	$delimiter = readline("delimiter:");
-
-	//$alphabet = getAlphabet($key);
 
 	$txt = "";
 
 	foreach($file as $line){
 		$txt.= useKey($line,$key,$delimiter);
 	}
+
 	if(readline("save ?(s/n)") == "s"){
 		file_put_contents(readline("file:"),$txt,FILE_APPEND);
 	}else{
 		echo $txt;
 	}
-
 }
 
-test();
+lock();
