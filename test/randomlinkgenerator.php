026d85fa4b3485aa81269c0f33d125b9b6d88216
allansm <allan.de.souza.melo.rj@gmail.com>
2021-04-15 21:50:29 -0300

    ...

--- test/randomlinkgenerator.php
+++ test/randomlinkgenerator.php
@@ -1,10 +1,19 @@
 <?php
 
+function createFile($fname){
+	fopen($fname, "w");
+	fclose($fname);
+}
 
-function generate(){
+function copyLinks(){
+	if(file_exists("links.txt")){
+		unlink("links.txt");
+	}
 	$linkspath = file(".config")[0];
 
 	$links = file($linkspath);
+
+	createFile("links.txt");
 
 	foreach($links as $link){
 		file_put_contents("links.txt",$link,FILE_APPEND);
@@ -26,3 +35,31 @@ function consumeLine($fname,$index){
 	
 	return $ret;
 }
+
+function resolveLink($link){
+	$command = file(".config")[1];
+	$command.=" ".$link." > redirect.txt";
+
+	exec($command);
+}
+
+
+function run(){
+	if(file_exists("links.txt")){
+		try{
+			$link = consumeLine("links.txt",0);
+		}catch(exception $ex){
+			copyLinks();
+			$link = consumeLine("links.txt",0);
+		}
+		if($link == ""){
+			copyLinks();
+		}else{
+			resolveLink($link);
+		}
+	}else{
+		copyLinks();
+	}
+}
+while(true)
+	run();
