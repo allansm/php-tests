dd331b045a32ceb362666d86a81679204a637d0c
allansm <allan.de.souza.melo.rj@gmail.com>
2021-04-17 20:13:42 -0300

    ...

--- sfchat/sendmsg.php
+++ sfchat/sendmsg.php
@@ -5,23 +5,36 @@ include("../functions/util.php");
 
 
 function sender($msg){
-	//while(true){
-		//$msg = $argv[1];
-		if($msg == "cls"){
-			get_remote_data(removeLineBreak(file("url.txt")[0]).$msg);
-		}else{
-			//$msg = argv[1];//readline("msg:");
-
-			$msg = file("username.txt")[0].":".$msg;
-			$msg.="\n";
-			$encmsg = usePattern($msg,file("key.txt")[0],"-");
-			$url = removeLineBreak(file("url.txt")[0]).$encmsg;
-			if(strlen($url) < 2048){
-				//file_get_contents($url);
-				$ht = get_remote_data($url);
-			}
+	if($msg == "cls"){
+		get_remote_data(removeLineBreak(file("url.txt")[0]).$msg);
+	}else{
+		$msg = file("username.txt")[0].":".$msg;
+		$msg.="\n";
+		$encmsg = usePattern($msg,file("key.txt")[0],"-");
+		$url = removeLineBreak(file("url.txt")[0]).$encmsg;
+		if(strlen($url) < 2048){
+			$ht = get_remote_data($url);
 		}
-	//}
+	}
 }
 
+function receiver(){
+	$chat = get_remote_data(removeLineBreak(file("msgloc.txt")[0]));
+	
+	$exp = explode("\n",$chat);
+	foreach($exp as $line){
+		$chat = useKey($line,file("key.txt")[0],"-");
+		print($chat);
+	}
+
+	sleep(rand(1,10));
+
+	clean();
+
+	receiver();
+}
+if($argv[1] == "showChat"){
+	receiver();
+	die();
+}
 sender($argv[1]);
