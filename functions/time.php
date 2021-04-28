83a67464aa622fb3442d8259fa52fbdf95e56800
allansm <allan.de.souza.melo.rj@gmail.com>
2021-04-28 20:05:47 -0300

    ...

--- functions/time.php
+++ functions/time.php
@@ -10,3 +10,12 @@ function elapsed($sttime){
 function toSec($millis){
 	return intval($millis/1000);
 }
+
+function toMinute($millis){
+	$sec = toSec($millis);
+	return intval($sec/60);
+}
+function toHour($millis){
+	$min = toMinute($millis);
+	return intval($min/60);
+}
---
+++ timer/timer.php
@@ -0,0 +1,28 @@
+<?php
+
+include("../functions/time.php");
+include("../functions/util.php");
+
+
+$wait = readline("time in minute:");
+$start = timeToMillis();
+
+while(true){
+	clean();
+
+	$hour = toHour(elapsed($start));
+	$minute = toMinute(elapsed($start)) - (60*$hour);
+	$sec = toSec(elapsed($start)) - (60*$minute);
+	$clock = $hour . ":" . $minute . ":" . $sec;
+	
+	print("waiting:".$wait."\n");
+	print("timer:".$clock."\n");
+
+	if(intval($wait) <= toMinute(elapsed($start))){
+		print("time reached.");
+		exec("ffplay -nodisp -autoexit -loglevel 0 1.wav");
+	}else{
+		sleep(1);
+	}
+}
+
