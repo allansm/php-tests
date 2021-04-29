8aab401198fa14639f0d719d4ac5bae8c35e8c6a
allansm <allan.de.souza.melo.rj@gmail.com>
2021-04-29 20:57:48 -0300

    ...

---
+++ timer/wt.php
@@ -0,0 +1,34 @@
+<?php
+
+include("../functions/time.php");
+include("../functions/util.php");
+include( "..\\functions\\fileHandle.php");
+
+
+$start = timeToMillis();
+$elapsedBefore = 0;
+
+$date = getdate();
+if(isset($argv[1])){
+	$fn = "data/".$argv[1];
+}else{
+	$fn = "data/".$date["mday"].$date["mon"].$date["year"];
+}
+
+if(file_exists($fn)){
+	$elapsedBefore = intval(file($fn)[0]);
+}
+
+while(true){
+	clean();
+
+	$hour = toHour(elapsed($start)+$elapsedBefore);
+	$minute = toMinute(elapsed($start)+$elapsedBefore) - (60*$hour);
+	$sec = toSec(elapsed($start)+$elapsedBefore) - (60*$minute);
+	$clock = $hour . ":" . $minute . ":" . $sec;
+
+	exec("echo ".(elapsed($start)+$elapsedBefore)." > ".$fn);
+
+	echo $fn." ".$clock;
+	sleep(1);
+}
