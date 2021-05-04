219da4de01ddfc8d44c04d16fcf5551829df7295
allansm <allan.de.souza.melo.rj@gmail.com>
2021-05-04 20:22:07 -0300

    ...

--- streamscrapper/test.php
+++ streamscrapper/test.php
@@ -1,6 +1,7 @@
 <?php
 
 include("import/ttodua.php");
+include("../functions/util.php");
 
 function find($html,$p1,$p2){
 	$p1 = str_replace("/", "\/", $p1);
@@ -35,19 +36,42 @@ function avaible($fname){
 	$lines = file($fname);
 	$i = 0;
 	foreach($lines as $line){
-		print($i.":".explode("^",$line)[0]."\n");
+		print($i++.":".explode("^",$line)[0]."\n");
 	}
 }
 function showstreams($pn){
 	$line = file("data/patterns.txt")[$pn];
 	$exp = explode("^",$line);
-	$streams = getstreams(get_remote_data($exp[0]),$exp[1],$exp[2]);
+	$streams = getstreams(get_remote_data(removeLineBreak($exp[0])),removeLineBreak($exp[1]),removeLineBreak($exp[2]));
 	$i=0;
 	foreach($streams as $stream){
 		print($i++.":".$stream."\n");
 	}
 }
+function play($n,$pn){
+	$line = file("data/patterns.txt")[$pn];
+	$exp = explode("^",$line);
+	$streams = getstreams(get_remote_data(removeLineBreak($exp[0])),removeLineBreak($exp[1]),removeLineBreak($exp[2]));
+	$i=0;
+	foreach($streams as $stream){
+		$room = $stream;
+		if($n == $i++){
+			while(true){
+				exec("youtube-dl -f \"bestvideo[height<=720]+bestaudio/best[height<=720]\" --get-url ".$room." > data/room");
+				$url = file("data/room")[0];
+				print("watching:".$room."\n");
+				exec("echo ".$room." > data/.log");
+				if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+					exec("ffplay -an -x 300 -y 170 -top 28 -left 1000 -noborder -alwaysontop -framedrop -autoexit -fflags nobuffer -loglevel 0 ".$url);
+				}else{
+					exec("ffplay -an -x 300 -y 170  -framedrop -autoexit -fflags nobuffer -loglevel 0 ".$url);
+				}
+			}	
+		}
+	}
+
+}
 avaible("data/patterns.txt");
-showstreams(readline("n:"));
-//$site = readline("site:");
-//print_r(getstreams(get_remote_data($site),$site,readline("keyword:")));
+$pn = readline("n:");
+showstreams($pn);
+play(readline("n:"),$pn);
