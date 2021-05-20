2bcf884834a77421e2b80d4c21b95956be38ea6f
allansm <allan.de.souza.melo.rj@gmail.com>
2021-05-20 16:04:48 -0300

    ...

--- functions/ff.php
+++ functions/ff.php
@@ -3,7 +3,7 @@
 function player($screen,$mp4){
 	if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
 		if($screen == ""){
-			exec("ffplay -an -x 300 -y 170 -top 28 -left 1000 -noborder -alwaysontop -framedrop -autoexit -loglevel 0 ".$mp4);
+			exec("ffplay -an -x 300 -y 170 -top 28 -left 1000 -noborder -framedrop -alwaysontop -autoexit -loglevel 0 ".$mp4);
 		}else{
 			$size = explode("x",$screen);
 			exec("ffplay -an -x ".$size[0]." -y ".$size[1]." -noborder -framedrop -autoexit -fflags nobuffer -loglevel 0 ".$mp4);	
