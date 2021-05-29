170b8c080f2a63919cfe29b381c0b49d4dd3ecf6
allansm <allan.de.souza.melo.rj@gmail.com>
2021-05-29 20:09:26 -0300

    ...

--- getsource/downloadImages.php
+++ getsource/downloadImages.php
@@ -5,21 +5,6 @@ include("../functions/util.php");
 include("../functions/fileHandle.php");
 include("getsourceFunctions.php");
 
-/*
-function download($url,$folder){
-	$file_name = basename($url);
-	
-	$file_name = $folder.$file_name;
-
-	if(file_put_contents( $file_name,file_get_contents($url))) {
-		echo "File downloaded successfully\n";
-	}
-	else {
-		echo "File downloading failed.\n";
-	}
-	           
-}
- */
 
 $page = get_remote_data($argv[1]);
 $page = str_replace("<","\n",$page);
@@ -30,17 +15,10 @@ print($page."\n");
 
 $lines = getLines($page);
 
-/*
-chdir(sys_get_temp_dir());
-
-createFolder("getsource");
-chdir("getsource");
- */
 toGetSource();
 
 foreach($lines as $line){
-	if(hasPattern($line,"http;src=;.jpg") || hasPattern($line,"http;src=;.png") || hasPattern($line,"http;src=;.gif")){
-		
+	if(hasPattern($line,"http;src=;.jpg") || hasPattern($line,"http;src=;.png") || hasPattern($line,"http;src=;.gif")){		
 		$url = find($line,"src=\"","\"");
 		if(has($url,".jpg") || has($url,".png") || has($url,".git")){
 			print("$url\n\n");
@@ -48,7 +26,6 @@ foreach($lines as $line){
 		}
 	}
 	if(hasPattern($line,"http;data-src=;.jpg") || hasPattern($line,"http;data-src=;.png") || hasPattern($line,"http;data-src=;.gif")){
-		
 		$url = find($line,"data-src=\"","\"");
 		if(has($url,".jpg") || has($url,".png") || has($url,".git")){
 			print("$url\n\n");
