5096259a390421937f1fa7df3f037fd7c8015b37
allansm <allan.de.souza.melo.rj@gmail.com>
2021-06-05 20:12:51 -0300

    ...

--- timer/timer.php
+++ timer/timer.php
@@ -17,13 +17,6 @@ function persist($msg){
 	}
 }
 
-/*
-if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-	exec("echo waiting to $title waittime:$wait time:%date% %time% >> data/.log");	
-}else{
-	exec("echo waiting to $title time:$wait >> data/.log");
-}
- */
 
 persist("waiting to $title waittime:$wait");
 
