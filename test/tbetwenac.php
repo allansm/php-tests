669d9ae7e60b24b9e246bbd83c82ec9b7eb83560
allansm <allan.de.souza.melo.rj@gmail.com>
2021-06-06 22:33:26 -0300

    ...

--- test/tbetwenac.php
+++ test/tbetwenac.php
@@ -4,15 +4,12 @@ include("../functions/time.php");
 
 $start = timeToMillis();
 
-sleep(rand(1,2));
-sleep(rand(1,2));
-sleep(rand(1,2));
-sleep(rand(1,2));
-sleep(rand(1,2));
+sleep(rand(1,60*20));
 
 
-if(toSec(elapsed($start)) > 7){
+if(toSec(elapsed($start)) >= 60*15){
 	echo "ok";
 }else{
+	echo toSec(elapsed($start));
 	readline("this is a security lock press any key");
 }
