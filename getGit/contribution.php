4c6771b1d7a59a4171b560f8f5e5a0ad4ef7fa2d
allansm <allan.de.souza.melo.rj@gmail.com>
2021-06-22 22:11:46 -0300

    ...

--- getGit/contribution.php
+++ getGit/contribution.php
@@ -1,7 +1,35 @@
 <?php
 include("git.php");
 
-$graph = yearGraphContribution(getContribution($argv[1]));
+$contribution = getContribution($argv[1]);
+
+$graph = yearGraphContribution($contribution);
 
 print("\n");
 print($graph);
+
+
+print("\n\ncontributions:\n\n");
+
+foreach(getLast($contribution) as $last){
+	print($last."\n");
+}
+
+print("\n");
+
+
+print("average contribution:".round(averageYear($contribution))."\n");
+$avarageLevel = avarageLevelYear($contribution);
+$avarageLevel = round($avarageLevel);
+$avarageLevel = str_replace("0","x",$avarageLevel);
+$avarageLevel = str_replace("1","<",$avarageLevel);
+$avarageLevel = str_replace("2","=",$avarageLevel);
+$avarageLevel = str_replace("3",">",$avarageLevel);
+$avarageLevel = str_replace("4","+",$avarageLevel);
+
+print("avarage concept : $avarageLevel\n");
+print("highest contribution:".highest($contribution)."\n");
+print("total contribution:".totalContribution($contribution)."\n");
+print("total contribution this year:".totalYearContribution($contribution));
+
+print("\n");
