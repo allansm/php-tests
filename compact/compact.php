b77b21e113c3824dbdff5782fdfd7c78b508e6bb
allansm <allan.de.souza.melo.rj@gmail.com>
2021-06-30 20:30:57 -0300

    ...

--- compact/compact.php
+++ compact/compact.php
@@ -3,21 +3,7 @@
 function getsizemb($file){
 	return round((filesize($file)/1024)/1024);
 }
-/*deprecated
-function addFile($toadd,$file){
-	$data = implode("",file($toadd));
-		
-	$toadd = basename($toadd);
 
-	file_put_contents($file,bin2hex("$toadd"),FILE_APPEND);
-	file_put_contents($file,"@",FILE_APPEND);
-	
-	file_put_contents($file,bin2hex($data),FILE_APPEND);
-
-	file_put_contents($file,"#",FILE_APPEND);
-
-}
- */
 function addFileTest($toadd,$file){
 	$data = fopen($toadd,"rb");
 	
@@ -34,22 +20,7 @@ function addFileTest($toadd,$file){
 
 	fclose($data);
 }
-/*deprecated
-function extractFiles($file){
-	$data = file($file)[0];
-	$files = explode("#",$data);
-	foreach($files as $tmp){
-		$tmp2 = explode("@",$tmp);
-		$tmp2 = array_diff($tmp2,[""," "]);
-		$tmp2 = array_values($tmp2);
 
-		if(array_key_exists(0,$tmp2)){
-			$fn = hex2bin($tmp2[0]);	
-			file_put_contents($fn,hex2bin($tmp2[1]),FILE_APPEND);	
-		}
-	}
-}
- */
 function extractFilesTest($file){
 	$data = fopen($file,"rb");
 	$fname = "";
@@ -82,23 +53,6 @@ function extractFilesTest($file){
 	fclose($data);
 }
 
-//deprecated
-/*function extractFile($index,$file){
-	$data = file($file)[0];
-	$files = explode("#",$data);
-	$i = 0;
-	foreach($files as $tmp){
-		$tmp2 = explode("@",$tmp);
-		$tmp2 = array_diff($tmp2,[""," "]);
-		$tmp2 = array_values($tmp2);
-			
-		if($i++ == $index)
-		if(array_key_exists(0,$tmp2)){
-			$fn = hex2bin($tmp2[0]);	
-			file_put_contents($fn,hex2bin($tmp2[1]),FILE_APPEND);	
-		}
-	}
-}*/
 
 function extractFileTest($index,$file){
 	$data = fopen($file,"rb");
@@ -134,25 +88,6 @@ function extractFileTest($index,$file){
 	fclose($data);
 }
 
-
-/*
-function showFiles($file){
-	$data = file($file)[0];
-	$files = explode("#",$data);
-	$arr = [];
-	foreach($files as $tmp){
-		$tmp2 = explode("@",$tmp);
-		$tmp2 = array_diff($tmp2,[""," "]);
-		$tmp2 = array_values($tmp2);
-
-		if(array_key_exists(0,$tmp2)){
-			$fn = hex2bin($tmp2[0]);
-			array_push($arr,$fn);
-		}
-	}
-	print_r($arr);
-}
- */
 
 function showFilesTest($file){
 	$data = fopen($file,"rb");
