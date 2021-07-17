fab0ac40c817e6cb5e58b25e7122be3653e548aa
allansm <allan.de.souza.melo.rj@gmail.com>
2021-07-17 00:48:03 -0300

    ...

--- test/buffertest.php
+++ test/buffertest.php
@@ -48,28 +48,57 @@ function test3(){
 	$file = fopen("test.txt","rb");
 	$b = 1;
 	$bc = 0;
-	$loc = array();
+	$loc = [];
+	$start = 0;
+	$i = 0;
+	$txt = "";
 	while(!feof($file)){
 		$char = fread($file,$b);
+		$txt.=$char;
 		if($char == "#"){
-			array_push($loc,$bc);	
+			$loc[$i]["start"] = $start;
+			$loc[$i]["end"] = $bc;
+			$loc[$i++]["len"] = strlen($txt);
+			$start = $bc;
+			$txt = "";
 		}
-		$bc+=$b;
+		if($char != ""){
+//			echo "$char $bc\n";
+			$bc+=$b;
+			//$lastchar = $char;
+		}
 	}
 	print_r($loc);
 }
 
 function test4($skip,$b){
 	$file = fopen("test.txt","rb");
-	if($skip != 0){
-		$skip = fread($file,$skip);
-
-	}
 	while(!feof($file)){
+		if($skip != 0){
+			$skip+=1;
+			$skipped = fread($file,$skip);
+
+		}
+		$b = $b-$skip;
+		if($skip == 0){
+			$b-=1;
+		}/*else{
+			$b+=1;
+			}*/
 		$text = fread($file,$b);
 		print($text);
+		fclose($file);
 		break;
 	}	
+}
+
+function test5(){
+	$file = fopen("test.txt","rb");
+	$arr = [];
+	while(!feof($file)){
+		array_push($arr,fread($file,1));
+	}
+	print_r($arr);
 }
 
 function hugeText(){
@@ -77,12 +106,15 @@ function hugeText(){
 	for($i = 0;$i<=10240000;$i++){
 		$huge.="9";
 	}
+	return $huge;
 }
 //hugeText();
 //test();
 test3();
-
+//echo hugeText();
 test4(0,11);
-test4(11,21);
-
-
+print("\n");
+test4(10,16);
+print("\n");
+test4(16,19);
+//test5();
