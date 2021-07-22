7f98c57b03c2bbe89813cb334945bb41c9efc6ea
allansm <allan.de.souza.melo.rj@gmail.com>
2021-07-21 21:26:18 -0300

    ...

--- compact/test.php
+++ compact/test.php
@@ -8,20 +8,23 @@ if(array_key_exists(2,$argv)){
 	$wf = $argv[2];
 }
 
-$options = array("--add","--show","--extract","--extract-all");
+function run($op,$wf){
+	$options = array("--add","--show","--extract","--extract-all");
 
-if($op == "--add"){
-	$toadd = readline("to add:");
-	addFile($toadd,$wf);
-}else if($op == "--show"){
-	print_r(showFiles($wf));
-}else if($op == "--help"){
-	foreach($options as $o){
-		print("$o\n");
+	if($op == "--add"){
+		$toadd = readline("to add:");
+		addFile($toadd,$wf);
+	}else if($op == "--show"){
+		print_r(showFiles($wf));
+	}else if($op == "--help"){
+		foreach($options as $o){
+			print("$o\n");
+		}
+	}else if($op == "--extract-all"){
+		extractFiles($wf);
+	}else if($op == "--extract"){
+		extractByName($wf,$argv[3]);
 	}
-}else if($op == "--extract-all"){
-	extractFiles($wf);
-}else if($op == "--extract"){
-	extractByName($wf,$argv[3]);
 }
 
+run($op,$wf);
